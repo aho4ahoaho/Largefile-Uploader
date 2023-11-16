b5eda4cd3290e945a57e131ad6c7a73ae1648a0e
aho4ahoaho <aho4ahoaho@outlook.jp>
2023-11-17 03:03:59 +0900

    コメントと人力フォーマット

--- static/script.js
+++ static/script.js
@@ -1,15 +1,15 @@
 window.onload = () => {
   const fileInput = document.getElementById("file");
   const submitBtn = document.getElementById("button");
-  const resiltDiv = document.getElementById("result");
+  const resultDiv = document.getElementById("result");
 
   submitBtn.addEventListener("click", async () => {
-    console.log("clicked");
     const file = fileInput.files[0];
 
-    const uploader = new SplitUploader(file, 100 * 1024 ** 2, 3);
+    const uploader = new SplitUploader(file, 30 * 1024 ** 2, 3);
     await uploader.uploadParallel();
     await uploader.concat();
+    resultDiv.textContent = "アップロード完了";
   });
 };
 
@@ -17,6 +17,7 @@ class SplitUploader {
   maxRetries = 3;
   prefix;
   maxParallelUploads = 3;
+  indexPadding = 3;
   /**
    *
    * @param {File} file アップロードするファイル
@@ -25,19 +26,27 @@ class SplitUploader {
   constructor(file, chunkSize = 100 * 1024 ** 2) {
     this.file = file;
     this.chunkSize = chunkSize;
-    console.log("chunkSize", chunkSize);
+
+    // 分割数
     this.totalChunks = Math.ceil(file.size / chunkSize);
+    // 分割数の桁数
+    this.indexPadding = (this.totalChunks - 1).toString().length;
+    // 現在の分割数
     this.chunkIndex = 0;
+    // ファイル名のプレフィックスの取得
     this.getPrefix().then((prefix) => {
       this.prefix = prefix;
     });
   }
 
   async getPrefix() {
+    // 拡張子の取得
     const ext = this.file.name.split(".").pop();
     if (!ext) {
       throw new Error("Failed to get extension");
     }
+
+    // プレフィックスの取得
     const prefix = await fetch(`/upload/prefix?ext=${ext}`)
       .then((res) => {
         if (res.status === 200) {
@@ -49,9 +58,13 @@ class SplitUploader {
     if (!prefix) {
       throw new Error("Failed to get prefix");
     }
+    console.debug("prefix", prefix);
     return prefix;
   }
 
+  /**
+   * ファイルの結合
+   */
   async concat() {
     const res = await fetch(`/upload/concat`, {
       method: "POST",
@@ -65,12 +78,10 @@ class SplitUploader {
     if (res.status !== 200) {
       throw new Error("Failed to concat");
     }
-    console.log("Concat done");
     return res;
   }
 
   getNextChunk() {
-    console.log("getNextChunk", this.chunkIndex, this.totalChunks);
     const index = this.chunkIndex;
     if (this.chunkIndex < this.totalChunks) {
       const start = index * this.chunkSize;
@@ -83,28 +94,34 @@ class SplitUploader {
   }
 
   async uploadChunk(chunk, index) {
-    console.log("start upload chunk", index);
-    const formData = new FormData();
+    // prefixが取得できるまで待機
     while (!this.prefix) {
       await new Promise((resolve) => setTimeout(resolve, 100));
     }
-    formData.append("file", chunk, this.prefix + ".part." + index);
-    console.log(this.prefix + ".part." + index);
+
+    const formData = new FormData();
+    const filename = `${this.prefix}.part.${index
+      .toString()
+      .padStart(this.indexPadding, "0")}`;
+    formData.append("file", chunk, filename);
+
     for (let i = 0; i < this.maxRetries; i++) {
       const res = await fetch("/upload", {
         method: "POST",
         body: formData,
       });
       if (res.status === 200) {
-        console.log("Uploaded chunk", index);
         return;
       }
     }
+
     throw new Error("Failed to upload chunk");
   }
 
+  /**
+   * 逐次アップロード
+   */
   async upload() {
-    console.log("upload", this.chunkIndex, this.totalChunks);
     const [chunk, index] = this.getNextChunk();
     if (chunk) {
       await this.uploadChunk(chunk, index);
@@ -112,13 +129,10 @@ class SplitUploader {
     }
   }
 
+  /**
+   * 並列アップロード
+   */
   async uploadParallel() {
-    console.log(
-      "uploadParallel",
-      this.chunkIndex,
-      this.totalChunks,
-      this.maxParallelUploads
-    );
     const chunks = [];
     for (let i = 0; i < this.maxParallelUploads; i++) {
       const [chunk, index] = this.getNextChunk();
@@ -126,7 +140,9 @@ class SplitUploader {
         chunks.push(this.uploadChunk(chunk, index));
       }
     }
+
     await Promise.all(chunks);
+
     if (this.chunkIndex < this.totalChunks) {
       await this.uploadParallel();
     }
